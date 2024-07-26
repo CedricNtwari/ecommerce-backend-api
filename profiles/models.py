16180ccdcff18be3680dbfa126aa1ab28a7bb562
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-26 17:50:38 +0200

    feat: Update Profile model to include address and phone number fields

--- profiles/models.py
+++ profiles/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
+from django_countries.fields import CountryField
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
-    address = models.TextField(blank=False)  # Ensuring the address is not empty
-    phone_number = PhoneNumberField(blank=True, region="US")  # Using django-phonenumber-field
+    street_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255, blank=True)
+    postal_code = models.CharField(max_length=20)
+    country = CountryField(blank_label='(select country)')
+    phone_number = PhoneNumberField(blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_profile_xffzir'
-    )
+    image = models.ImageField(upload_to='images/', default='../default_profile_xffzir')
 
     class Meta:
         ordering = ['-created_at']
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,17 +1,36 @@
 from rest_framework import serializers
+from phonenumbers import parse, is_valid_number, NumberParseException
 from .models import Profile
+from django_countries.serializer_fields import CountryField  # Import CountryField
 
+EU_COUNTRY_CODES = [
+    "AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", 
+    "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", 
+    "SI", "ES", "SE"
+]
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    country = CountryField()
+
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'address', 'phone_number',
-            'content', 'image', 'is_owner'
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'street_address', 'city', 
+            'state', 'postal_code', 'country', 'phone_number', 'content', 'image', 'is_owner'
         ]
     
     def get_is_owner(self, obj):
         request = self.context.get('request')
         return request.user == obj.owner if request else False
+
+    def validate_phone_number(self, value):
+        for country_code in EU_COUNTRY_CODES:
+            try:
+                phone_number = parse(value, country_code)
+                if is_valid_number(phone_number):
+                    return value
+            except NumberParseException:
+                continue
+        raise serializers.ValidationError("The phone number is not valid for any of the EU countries.")
