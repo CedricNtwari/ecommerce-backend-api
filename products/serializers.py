89302120b2183de0ddbe92b1e1dac2eb3a460458
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-26 11:30:10 +0200

    feat: Add ProductSerializer for Product model

---
+++ products/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError('Image height larger than 4096px!')
+        if value.image.width > 4096:
+            raise serializers.ValidationError('Image width larger than 4096px!')
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner if request else False
+
+    class Meta:
+        model = Product
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'created_at', 'updated_at', 'name', 'description', 'price',
+            'stock', 'image', 'image_filter'
+        ]
