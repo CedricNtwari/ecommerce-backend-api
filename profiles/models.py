da0a10075a9dc0e27756bf93c767827bdf3b7014
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-26 17:17:58 +0200

    feat: Integrate django-phonenumber-field for phone number validation

--- profiles/models.py
+++ profiles/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
-    address = models.TextField(blank=True)
-    phone_number = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=False)  # Ensuring the address is not empty
+    phone_number = PhoneNumberField(blank=True, region="US")  # Using django-phonenumber-field
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_xffzir'
