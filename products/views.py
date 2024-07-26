b809851145b50e247fcfb92c2ee94f2bc878c8e6
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-26 11:30:36 +0200

    feat: Add ProductList and ProductDetail views

---
+++ products/views.py
@@ -0,0 +1,36 @@
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions, filters
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductList(generics.ListCreateAPIView):
+    """
+    List products or create a product if logged in
+    The perform_create method associates the product with the logged in user.
+    """
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Product.objects.all().order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'owner__profile',
+    ]
+    search_fields = ['owner__username', 'name']
+    ordering_fields = ['created_at']
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a product and edit or delete it if you own it.
+    """
+    serializer_class = ProductSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Product.objects.all()
