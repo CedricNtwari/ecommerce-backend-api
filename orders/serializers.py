218dddc3200d31bde480fa312b6e23b3ca3c5231
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-27 23:01:27 +0200

    feat: Add serializers for Order and OrderItem

---
+++ orders/serializers.py
@@ -0,0 +1,28 @@
+# In orders/serializers.py
+
+from rest_framework import serializers
+from .models import Order, OrderItem
+from products.models import Product
+from profiles.serializers import ProfileSerializer
+import uuid
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'quantity', 'price']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    owner = serializers.ReadOnlyField(source='owner.username')
+    profile = ProfileSerializer(source='owner.profile', read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'order_number', 'owner', 'profile', 'total_price', 'created_at', 'updated_at', 'status', 'items']
+        read_only_fields = ['order_number']
+
+    def create(self, validated_data):
+        validated_data['order_number'] = str(uuid.uuid4()).replace("-", "").upper()[:20]
+        return super().create(validated_data)
