e63b480e96304b1eb03061e147d17d2d93f94228
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-29 14:27:24 +0200

    Introduced Cart and CartItem models to manage user carts and items within carts

---
+++ cart/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+class Cart(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Cart of {self.owner.username}'
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
