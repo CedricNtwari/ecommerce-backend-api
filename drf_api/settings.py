8ab48a6711578d34a5eed19a22bf471efe7ee73e
CedricNtwari <cedric.ntwari@powercoders.org>
2024-08-01 14:49:10 +0200

    Fix: avoided duplication

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -78,6 +78,9 @@ JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
@@ -183,10 +186,6 @@ if 'DEV' not in os.environ:
         'rest_framework.renderers.JSONRenderer',
     ]
 
-REST_USE_JWT = True
-JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
