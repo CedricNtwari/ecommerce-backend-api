833f721938b23fb048ef288f610f66f50d512840
CedricNtwari <cedric.ntwari@powercoders.org>
2024-09-19 10:08:20 +0200

    fix(middleware): remove incorrect allauth account middleware reference

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,8 +61,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 # CORS Configuration
